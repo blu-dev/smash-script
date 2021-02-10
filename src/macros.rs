46c810569cb7259a2a4ad5d94655eda705c3da89
blu-dev <the.blu.dev@gmail.com>
2021-02-09 18:28:18 -0700

    **untested** include Hash40::new("no") via nan

--- src/macros.rs
+++ src/macros.rs
@@ -60,7 +60,9 @@ pub unsafe fn ATTACK<A: ToF32, B: ToF32, C: ToF32>(fighter: &mut L2CAgentBase, i
     if let Some(x2) = x2 { lua_args!(fighter, x2); } else { fighter.push_lua_stack(&mut L2CValue::new()); }
     if let Some(y2) = y2 { lua_args!(fighter, y2); } else { fighter.push_lua_stack(&mut L2CValue::new()); }
     if let Some(z2) = z2 { lua_args!(fighter, z2); } else { fighter.push_lua_stack(&mut L2CValue::new()); }
-    lua_args!(fighter, hitlag, sdi, clang, facing, set_weight, shield_damage.to_f32(), trip, rehit.to_f32(), reflectable, absorbable, flinchless, disable_hitlag, direct, ground_air, hitbits, collision_part, friendly_fire, effect, sfx_level, collision_sound, _type);
+    lua_args!(fighter, hitlag, sdi, clang, facing, set_weight);
+    if (shield_damage.to_f32().is_nan()) { fighter.push_lua_stack(Hash40::new("no").into()); } else { fighter.push_lua_stack(shield_damage.to_f32().into()); }
+    lua_args!(fighter, trip, rehit.to_f32(), reflectable, absorbable, flinchless, disable_hitlag, direct, ground_air, hitbits, collision_part, friendly_fire, effect, sfx_level, collision_sound, _type);
     sv_animcmd::ATTACK(fighter.lua_state_agent);
 }
 
