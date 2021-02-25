3eeaff5bd350dd60a87d87db254bae69d3e7b6c3
blu-dev <the.blu.dev@gmail.com>
2021-02-24 21:21:35 -0700

    status script stuff, yo

--- lua-macro/src/lib.rs
+++ lua-macro/src/lib.rs
@@ -1,18 +1,96 @@
 #![feature(asm)]
-
+#![feature(const_loop)]
+#![feature(const_if_match)]
 use syn::{self, punctuated};
 use syn::parse::{Parse, ParseStream};
 use syn::{parenthesized, bracketed, token, Token};
 use proc_macro::TokenStream;
 use proc_macro2::TokenStream as TokenStream2;
 use quote::{quote, ToTokens};
-use smash::hash40;
+// use smash::hash40;
 //use std::io::Write;
+
+// borrowed from skyline-smash
+macro_rules! reflect {
+    ($bits:expr, $value:expr) => {{
+        let mut reflection = 0;
+        let mut value = $value;
+        let mut i = 0;
+
+        while i < $bits {
+            if (value & 0x01) == 1 {
+                reflection |= 1 << (($bits - 1) - i)
+            }
+
+            value >>= 1;
+            i += 1;
+        }
+
+        reflection
+    }};
+}
+
+const fn make_table(poly: u32) -> [u32; 256] {
+    let mut table = [0; 256];
+    let top_bit = 1 << 31;
+    let mut byte;
+
+    let mut i = 0;
+    while i <= 255 {
+        byte = reflect!(8, i);
+
+        let mut value = byte << 24;
+
+        let mut j = 0;
+        while j < 8 {
+            if (value & top_bit) != 0 {
+                value = (value << 1) ^ poly
+            } else {
+                value <<= 1
+            }
+
+            j += 1;
+        }
+
+        value = reflect!(32, value);
+
+        table[i as usize] = value;
+
+        i += 1;
+    }
+
+    table
+}
+
+const IEEE_TABLE: [u32; 256] = make_table(0x04C11DB7);
+
+const fn crc32(bytes: &[u8]) -> u32 {
+    let mut value = !0u32;
+    let mut i = 0;
+    while i < bytes.len() {
+        value = (value >> 8) ^ (IEEE_TABLE[((value ^ (bytes[i] as u32)) & 0xFF) as usize]);
+        i += 1;
+    }
+
+    !value
+}
+
+const fn hash40(string: &str) -> u64 {
+    let bytes = string.as_bytes();
+
+    ((bytes.len() as u64) << 32) + crc32(bytes) as u64
+}
 
 struct ScriptAttrs {
     pub agent: syn::LitStr,
     pub scripts: Vec<syn::LitStr>,
     pub category: syn::Path
+}
+
+struct StatusAttrs {
+    pub agent: syn::LitStr,
+    pub status: syn::Path,
+    pub condition: syn::Path
 }
 
 #[derive(Debug, Clone)]
@@ -68,17 +146,13 @@ impl<Keyword: Parse, Item: Parse, Punctuation: Parse> Parse for BracketedList<Ke
     }
 }
 
-/* #[script(
-        agent = "mariod", 
-        script = ["game_specialn", "game_specialairn"],
-        game_share
-    )] */ 
-
 mod kw {
     syn::custom_keyword!(agent);
     syn::custom_keyword!(script);
     syn::custom_keyword!(scripts);
     syn::custom_keyword!(category);
+    syn::custom_keyword!(status);
+    syn::custom_keyword!(condition);
 }
 
 impl syn::parse::Parse for ScriptAttrs {
@@ -131,10 +205,59 @@ impl syn::parse::Parse for ScriptAttrs {
     }
 }
 
+impl syn::parse::Parse for StatusAttrs {
+    fn parse(input : syn::parse::ParseStream) -> syn::Result<Self> {
+        let look = input.lookahead1();
+        let agent: syn::LitStr = if look.peek(kw::agent) {
+            let MetaItem::<kw::agent, syn::LitStr> { item: string, .. } = input.parse()?;
+
+            string
+        } else {
+            return Err(look.error());
+        };
+
+        let _: syn::Token![,] = input.parse()?;
+        let look = input.lookahead1();
+
+        let status: syn::Path = if look.peek(kw::status) {
+            let MetaItem::<kw::status, syn::Path> { item: path, .. } = input.parse()?;
+
+            path
+        } else {
+            return Err(look.error());
+        };
+
+        let _: syn::Token![,] = input.parse()?;
+        let look = input.lookahead1();
+
+        let condition: syn::Path = if look.peek(kw::condition) {
+            let MetaItem::<kw::condition, syn::Path> { item: cond, .. } = input.parse()?;
+
+            cond
+        } else {
+            return Err(look.error());
+        };
+        Ok(Self {
+            agent: agent,
+            status: status,
+            condition: condition
+        })
+    }
+}
+
 #[proc_macro]
 pub fn replace_script(input: TokenStream) -> TokenStream {
     let ident = syn::parse_macro_input!(input as syn::Ident);
     let installer_name = quote::format_ident!("_lua_replace_script_install_{}", ident);
+    quote!(
+        unsafe { #installer_name(); }
+    ).into()
+}
+
+#[proc_macro]
+pub fn replace_status_script(input: TokenStream) -> TokenStream {
+    let ident = syn::parse_macro_input!(input as syn::Ident);
+    let installer_name = quote::format_ident!("_lua_replace_status_install_{}", ident);
     quote!(
         unsafe { #installer_name(); }
     ).into()
@@ -318,6 +441,54 @@ pub fn script(attr: TokenStream, item: TokenStream) -> TokenStream {
             b #0x8
             .byte 0xE5, 0xB1, 0x00, 0xB0
             "#);
+        }
+    ).to_tokens(&mut output);
+    installer_string.parse::<TokenStream2>().unwrap().to_tokens(&mut output);
+    output.into()
+}
+
+#[proc_macro_attribute]
+pub fn status(attr: TokenStream, item: TokenStream) -> TokenStream {
+    let attr = syn::parse_macro_input!(attr as StatusAttrs);
+    let item_clone = item.clone();
+    let usr_fn = syn::parse_macro_input!(item as syn::ItemFn);
+
+    let usr_fn_name = &usr_fn.sig.ident;
+    let _agent = attr.agent;
+    let _status = attr.status;
+    let _condition = attr.condition;
+
+    let bootstrapper_name = quote::format_ident!("_lua_replace_status_bootstrapper_{}", usr_fn_name); // still required because of exceptions
+    let usr_new_name = quote::format_ident!("_lua_replace_status_usr_{}", usr_fn_name);
+    let installer_name = quote::format_ident!("_lua_replace_status_install_{}", usr_fn_name);
+
+    let func_string = item_clone.to_string();
+    let func_string = func_string.replace(&format!("fn {}", usr_fn_name.to_string()), &format!("fn {}", usr_new_name.to_string()));
+    let func_string = "#[inline(always)]\n".to_owned() + &func_string;
+    let mut output: TokenStream2 = func_string.parse().unwrap();
+
+    let agent_name = _agent.value();
+    let agent_hash;
+    if agent_name.starts_with("0x") {
+        agent_hash = format!("smash::phx::Hash40::new_raw({})", agent_name);
+    }
+    else {
+        agent_hash = format!("smash::phx::Hash40::new(\"{}\")", agent_name);
+    }
+
+    let replace_string = format!("smash_script::replace_status_script({}, {}, {}, {});", agent_hash, _status.get_ident().unwrap().to_string(), _condition.get_ident().unwrap().to_string(), bootstrapper_name.to_string());
+    let mut installer_string = format!(r#"
+    #[allow(non_uppercase_globals)]
+    unsafe fn {}() {{
+        {}
+    }}
+    "#, installer_name.to_string(), replace_string);
+
+    quote!(
+        #[inline(never)]
+        #[allow(unused_unsafe)]
+        unsafe extern "C" fn #bootstrapper_name(fighter: &mut smash::lua2cpp::L2CFighterBase) -> smash::lib::L2CValue {
+            #usr_new_name(std::mem::transmute(fighter))
         }
     ).to_tokens(&mut output);
     installer_string.parse::<TokenStream2>().unwrap().to_tokens(&mut output);
